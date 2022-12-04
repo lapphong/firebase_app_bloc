0fc2b206293acaf58213daecfd8c6bacb204b8e6
lapphong <lapphong012@gmail.com>
2022-12-04 09:55:17 +0700

    compl profile repo

---
+++ lib/repositories/profile_repository.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../configs/db_config.dart';
+import '../models/models.dart';
+
+class ProfileRepository {
+  final FirebaseFirestore firebaseFirestore;
+  ProfileRepository({
+    required this.firebaseFirestore,
+  });
+
+  Future<User> getProfile({required String uid}) async {
+    try {
+      final DocumentSnapshot userDoc = await userRef.doc(uid).get();
+
+      if (userDoc.exists) {
+        final currentUser = User.fromDoc(userDoc);
+        return currentUser;
+      }
+
+      throw 'User not found';
+    } on FirebaseException catch (e) {
+      throw CustomError(
+        code: e.code,
+        message: e.message!,
+        plugin: e.plugin,
+      );
+    } catch (e) {
+      throw CustomError(
+        code: 'Exception',
+        message: e.toString(),
+        plugin: 'flutter_error/server_error',
+      );
+    }
+  }
+}
